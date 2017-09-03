d7abd6d0f6776112477f24c92dedaacf3d6d3b43
sonumax <sonumax2@gmail.com>
2017-09-03 17:30:12 +0300

    Add method for check validate users

--- src/main/java/system/controller/UserController.java
+++ src/main/java/system/controller/UserController.java
@@ -2,9 +2,11 @@ package system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import system.model.User;
 import system.service.UserService;
 
@@ -20,5 +22,23 @@ public class UserController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public @ResponseBody List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @RequestMapping(value = "/validate", method = RequestMethod.GET)
+    public ModelAndView validateUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userFromServer", new User());
+        modelAndView.setViewName("users_check_page");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/check", method = RequestMethod.POST)
+    public @ResponseBody String checkUser(@ModelAttribute("userFromServer") User getUser) {
+        for (User user: userService.getAllUsers()) {
+            if(user.getName().equals(getUser.getName()) && user.getPassword().equals(getUser.getPassword())) {
+                return "valid";
+            }
+        }
+        return "invalid";
     }
 }
